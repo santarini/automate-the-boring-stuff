b52aa65fb120f66a779e7b22cfeb4a18805c6bbd
Makoa <32736507+santarini@users.noreply.github.com>
2018-04-20 15:02:15 -1000

    Update assertionExample.py

--- assertionExample.py
+++ assertionExample.py
@@ -1,3 +1,3 @@
 spam = int(input())
 
-assert spam < 10, "WHOA DUDE! \nWhat the hell do you think you're doing? \nComing into my house, entering values that aren't less than 10!"
+assert spam >= 10, "WHOA DUDE! \nWhat the hell do you think you're doing? \nComing into my house, entering values that aren't less than 10!"

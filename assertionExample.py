2de29903aaf90771facb2000f4017045ac6d358d
Makoa <32736507+santarini@users.noreply.github.com>
2018-04-20 15:09:25 -1000

    Update assertionExample.py

--- assertionExample.py
+++ assertionExample.py
@@ -1,16 +1,16 @@
 ''
 'less than 10
-spam = int(input())
+spam = int(input("Number>\n"))
 
 assert spam >= 10, "WHOA DUDE!\nWhat the hell do you think you're doing?\nComing into my house, entering values less than 10!"
 
 ''
 'eggs and bacon
-eggs = input("What is eggs?")
-bacon = input("What da bacons?")
+eggs = input("What is eggs?\n")
+bacon = input("What da bacons?\n")
 
 assert eggs.lower() != bacon.lower(), "eggs no can the bacon, brah!"
 
 ''
-'aways assert
+'always assert
 assert 0>1, "I'M JUST ALWAYS MAD FOR NO REASON!"

2491858d62acc601aafbfd808d4baf651d422f95
Makoa <32736507+santarini@users.noreply.github.com>
2018-04-20 15:04:55 -1000

    Update assertionExample.py

--- assertionExample.py
+++ assertionExample.py
@@ -1,3 +1,10 @@
+'less than 10
 spam = int(input())
 
 assert spam >= 10, "WHOA DUDE! \nWhat the hell do you think you're doing? \nComing into my house, entering values that aren't less than 10!"
+
+'eggs and bacon
+eggs = input("What is eggs?")
+bacon = input("What da bacons?")
+
+assert eggs.lower() != bacon.lower(), "eggs no can the bacon, brah!"

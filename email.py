47f4833c0ec57f6227ca0b39a80b122ba4ae97dc
Makoa <32736507+santarini@users.noreply.github.com>
2018-04-23 18:29:25 -1000

    Update email.py

--- email.py
+++ email.py
@@ -3,6 +3,5 @@ smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
 smtpObj.ehlo()
 smtpObj.starttls()
 smtpObj.login('email@gmail.com', 'password')
-smtpObj.sendmail('email@gmail.com', 'recipient@website.com',
-'Subject: So long.\nDear Alice,\n\nSo long and thanks for all the fish.\n\nSincerely, Bob')
+smtpObj.sendmail('email@gmail.com', 'recipient@website.com', 'Subject: So long.\nDear Alice,\n\nSo long and thanks for all the fish.\n\nSincerely, Bob')
 smtpObj.quit()
